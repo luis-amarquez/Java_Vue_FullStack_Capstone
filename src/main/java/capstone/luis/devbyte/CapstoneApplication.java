d655806dc922e6e96166ee0ff300652a58d27384
Luis Morales Marquez <github@luismorales.dev>
2022-04-17 22:52:44 -0700

    Initial commit

---
+++ src/main/java/capstone/luis/devbyte/CapstoneApplication.java
@@ -0,0 +1,23 @@
+package capstone.luis.devbyte;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan
+public class CapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/test/java/capstone/luis/devbyte/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.luis.devbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
